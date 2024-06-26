bb4835b41a456fa4fb9b80c88fda01e9230c81e4
wdh970616 <wdh970616@naver.com>
2024-06-26 15:42:25 +0900

    [FEAT] 입력조건
    입력조건이 필요한 항목들에 조건 추가
    현재 아이디만 추가함

--- src/main/java/com/hgcastle/app/InsertApp.java
+++ src/main/java/com/hgcastle/app/InsertApp.java
@@ -1,9 +1,9 @@
 package com.hgcastle.app;
 
-
 import com.hgcastle.dto.MemberDTO;
 import com.hgcastle.service.InsertService;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -22,12 +22,12 @@ public class InsertApp {
         Scanner sc = new Scanner(System.in);
         MemberDTO newMember = new MemberDTO();
 
-        System.out.print("등록할 아이디 : ");
-        String id = sc.nextLine();
+        // 아이디 입력 메소드
+        String id = inputID();
         String email = id + "@naver.com";
 
-        System.out.print("등록할 비밀번호 : ");
-        String password = sc.nextLine();
+        // 비밀번호 입력 메소드
+        String password = inputPassword();
 
         System.out.print("등록할 회원명 : ");
         String name = sc.nextLine();
@@ -38,7 +38,7 @@ public class InsertApp {
         System.out.print("등록할 회원의 취미 : ");
         String hobby = sc.nextLine();
 
-        System.out.print("등록할 회원의 전화번호 ('-'없이 작성) : ");
+        // 휴대전화번호 입력받는 메소드
         String phone = inputPhone();
 
         System.out.print("등록할 회원의 주민등록번호 앞 6자리 : ");
@@ -64,16 +64,55 @@ public class InsertApp {
         return newMember;
     }
 
+    private static String inputID() {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println();
+            System.out.println("***** 아이디 생성 조건 *****");
+            System.out.println("1. 영어 소문자와 숫자로만 구성");
+            System.out.println("2. 최소 5, 최대 12 글자");
+            System.out.print("등록할 아이디 : ");
+            String id = sc.nextLine();
+            if (id.isEmpty()) {
+                System.out.println("아이디는 [필수항목]입니다.");
+            } else if (id.matches("[a-z0-9]{5,12}")) {
+                return id;
+            } else {
+                System.err.print("조건에 맞게 입력해주세요!\n");
+            }
+        } while (true);
+    }
+
+    private static String inputPassword() {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println();
+            System.out.print("등록할 비밀번호 : ");
+            String password = sc.nextLine();
+            if (password.matches("^[a-zA-Z0-9]{8,20}$")) {
+                return password;
+            } else if (password.matches("^[a-z0-9]$")) {
+                System.out.println("비밀번호는 최소 8자, 최대 20자 까지 가능합니다.");
+            } else {
+                System.out.println("비밀번호는 영어 소문자와 숫자만 사용 가능합니다.");
+            }
+        } while (true);
+    }
+
     private static String inputPhone() {
         Scanner sc = new Scanner(System.in);
 
         do {
             System.out.print("등록할 회원의 전화번호 ('-'없이 작성) : ");
             String phone = sc.nextLine();
-            if (phone.length() == 11) {
+            if (phone.matches("^[0-9]^{11}$")) {
                 return phone;
-            } else {
-                System.out.println("잘못 입력하셨습니다.");
+            } else if (phone.matches("^{11}")) {
+                System.out.println("숫자 외에는 입력할 수 없습니다.");
+            } else if (phone.charAt(0) != '0') {
+                System.out.println("잘못된 번호를 입력하셨습니다.");
             }
         } while (true);
     }
