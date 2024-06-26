8441c5bdf7d5eb6e5273afd3ac779ba3d9843b3c
wdh970616 <wdh970616@naver.com>
2024-06-26 12:02:41 +0900

    [FEAT] 등록
    멤버 등록 기능 작성 도중
    메인 브랜치의 컨트롤러 패키지를
    가져오기위해 중간에 커밋함.

--- src/main/java/com/hgcastle/app/InsertApp.java
+++ src/main/java/com/hgcastle/app/InsertApp.java
@@ -1,4 +1,62 @@
 package com.hgcastle.app;
 
+import com.hgcastle.common.MemberDTO;
+import com.hgcastle.service.InsertService;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class InsertApp {
+
+    public static void main(String[] args) {
+        System.out.println("============= 회원 등록 기능 어플리케이션 =============");
+        InsertService.insertMember(inputNewMember());
+    }
+
+    private static MemberDTO inputNewMember() {
+        Scanner sc = new Scanner(System.in);
+        MemberDTO newMember = new MemberDTO();
+
+        System.out.print("등록할 아이디 : ");
+        String id = sc.nextLine();
+        String email = id + "@naver.com";
+
+        System.out.print("등록할 비밀번호 : ");
+        String password = sc.nextLine();
+
+        System.out.print("등록할 회원명 : ");
+        String name = sc.nextLine();
+
+        System.out.print("등록할 닉네임 : ");
+        String nickname = sc.nextLine();
+
+        System.out.print("등록할 회원의 취미 : ");
+        String hobby = sc.nextLine();
+
+        System.out.print("등록할 회원의 전화번호 ('-'없이 작성) : ");
+        String phone = sc.nextLine();
+        System.out.print("등록할 회원의 주민등록번호 앞 6자리 : ");
+        String frontRRN = sc.nextLine();
+        System.out.print("등록할 회원의 주민등록번호 뒤 7자리 : ");
+        String backRRN = sc.nextLine();
+        String rrn = frontRRN + '-' + backRRN;
+
+        System.out.print("등록할 회원의 거주지 : ");
+        String address = sc.nextLine();
+
+        newMember.setId(id);
+        newMember.setPassword(password);
+        newMember.setName(name);
+        newMember.setNickname(nickname);
+        newMember.setHobby(hobby);
+        newMember.setPhone(phone);
+        newMember.setEmail(email);
+        newMember.setRrn(rrn);
+        newMember.setAddress(address);
+        newMember.setEntDate(LocalDate.now());
+
+        return newMember;
+    }
+
+
 }
--- src/main/java/com/hgcastle/service/InsertService.java
+++ src/main/java/com/hgcastle/service/InsertService.java
@@ -1,4 +1,17 @@
 package com.hgcastle.service;
 
+import com.hgcastle.common.MemberDTO;
+import com.hgcastle.mapper.MemberMapper;
+import org.apache.ibatis.session.SqlSession;
+
+import static com.hgcastle.common.Template.getSqlSession;
+
 public class InsertService {
+
+    private MemberMapper mapper;
+
+    public static void insertMember(MemberDTO memberDTO) {
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(MemberMapper.class);
+    }
 }
