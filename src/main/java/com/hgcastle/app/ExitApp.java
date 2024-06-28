e2f511d3f43d4a004fb778cec2e9c0d867908469
parkhayarn <s0224y92@gmail.com>
2024-06-28 17:42:12 +0900

    [FEAT] : 탈퇴 회원 아이디로 조회 후 해당 아이디가 맞는지 확인하는 코드 생성중

--- src/main/java/com/hgcastle/app/ExitApp.java
+++ src/main/java/com/hgcastle/app/ExitApp.java
@@ -11,27 +11,24 @@ public class ExitApp {
     private static ExitResult exitResult = new ExitResult();
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         ExitController exitController = new ExitController();
-
 
         do {
             System.out.println("============회원 탈퇴 시스템==============");
             exitController.selectMemberId(inputMemberId());
-            break;
 
         } while (true);
 
     }
 
     private static Map<String, String> inputMemberId() {
-        ExitController exitController = new ExitController();
 
         Scanner sc = new Scanner(System.in);
         Map<String, String> parameter = new HashMap<>();
-
-        System.out.println("탈퇴할 아이디를 입력하세요 : ");
         String id = sc.nextLine();
+
+        System.out.println("탈퇴할 아이디를 입력하세요 : " + id);
+
         parameter.put("id", id);
 
         return parameter;
--- src/main/java/com/hgcastle/controller/ExitController.java
+++ src/main/java/com/hgcastle/controller/ExitController.java
@@ -30,7 +30,30 @@ public class ExitController {
             exitResult.printMemberId(member);
         } else {
             exitResult.printErrorMessage("selectOne");
-        }
+        } while (true);
     }
+
+    public boolean isItRight() {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("이대로 등록을 진행하시겠습니까?");
+            System.out.print("1.예(Y)      2.아니오(N) : ");
+            String answer = sc.nextLine();
+            if (answer.equals("예") || answer.equals("Y") || answer.equals("y") || answer.equals("1")
+                    || answer.equals("네") || answer.equals("ㅇ") || answer.equals("ㅇㅇ") || answer.equals("ㄱㄱ")
+                    || answer.equals("ㄱ") || answer.equals("응") || answer.equals("어") || answer.equals("웅")) {
+                return true;
+            } else if (answer.equals("아니오") || answer.equals("N") || answer.equals("n") || answer.equals("2")
+                    || answer.equals("아니요") || answer.equals("ㄴ") || answer.equals("ㄴㄴ") || answer.equals("놉")
+                    || answer.equals("노") || answer.equals("시러")) {
+                return false;
+            } else {
+                exitResult.printErrorMessage("wron");
+                System.out.println();
+            }
+        } while (true);
+
+}
 
 }
