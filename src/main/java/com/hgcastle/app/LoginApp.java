4236ef1426a5b6ef44af202f8f01295695fd8494
wdh970616 <wdh970616@naver.com>
2024-06-28 10:51:10 +0900

    [FEAT] 로그인
    로그인 기능 구현

--- src/main/java/com/hgcastle/app/LoginApp.java
+++ src/main/java/com/hgcastle/app/LoginApp.java
@@ -1,4 +1,15 @@
 package com.hgcastle.app;
 
+import java.util.Scanner;
+
 public class LoginApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("====== 로그인 기능 ======");
+        System.out.print("아이디 : ");
+//        String id = inputId();
+        System.out.print("비밀번호 : ");
+//        String password = inputPassword();
+    }
 }
