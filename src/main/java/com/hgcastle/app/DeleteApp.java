ffe3e59b1bfcbc3d90f7cad9fa7635798cd7f78c
parkhayarn <s0224y92@gmail.com>
2024-06-28 15:26:10 +0900

    [FEAT] : 회원 삭제 기능 추가

---
+++ src/main/java/com/hgcastle/app/DeleteApp.java
@@ -0,0 +1,35 @@
+package com.hgcastle.app;
+
+import com.hgcastle.controller.DeleteController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DeleteApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DeleteController deleteController = new DeleteController();
+
+        do {
+            System.out.println("=========== 회원 삭제 시스템 =========");
+            deleteController.deleteMemberId(inputMemberId());
+            break;
+
+        } while (true);
+    }
+
+    private static Map<String, String> inputMemberId() {
+        DeleteController deleteController = new DeleteController();
+
+        Scanner sc = new Scanner(System.in);
+        Map<String, String> parameter = new HashMap<>();
+
+        System.out.println("삭제할 아이디를 입력하세요 : ");
+        String id = sc.nextLine();
+        parameter.put("id", id);
+
+        return parameter;
+
+    }
+}
--- src/main/java/com/hgcastle/common/Template.java
+++ src/main/java/com/hgcastle/common/Template.java
@@ -1,25 +1,15 @@
 package com.hgcastle.common;
 
-import com.hgcastle.mapper.MemberMapper;
-import org.apache.ibatis.datasource.pooled.PooledDataSource;
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.mapping.Environment;
-import org.apache.ibatis.session.Configuration;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
-import javax.imageio.IIOException;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Template {
-
-    private static String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static String URL = "jdbc:mysql://localhost/choisamo_db";
-    private static String USER = "ohgiraffers";
-    private static String PASSWORD = "ohgiraffers";
 
     private static SqlSessionFactory sqlSessionFactory;
 
