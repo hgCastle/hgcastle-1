4f072c7ba73ecc815d65597fccd19e0ed97c1001
parkhayarn <s0224y92@gmail.com>
2024-06-28 10:46:39 +0900

    [FEAT] 탈퇴 기능 추가

--- src/main/java/com/hgcastle/app/ExitApp.java
+++ src/main/java/com/hgcastle/app/ExitApp.java
@@ -15,10 +15,10 @@ public class ExitApp {
             System.out.println("============회원 탈퇴 시스템==============");
             System.out.println("1. 아이디로 탈퇴할 회원 조회하기 ");
             System.out.println("메뉴 관리 번호를 입력해주세요 : ");
-            String id = sc.nextLine();
+            int no = sc.nextInt();
 
-            switch (id) {
-                case "1": exitController.selectId(inputId());break;
+            switch (no) {
+                case 1 : exitController.selectMemberId(inputId());break;
             }
         } while (true);
 
--- src/main/java/com/hgcastle/controller/ExitController.java
+++ src/main/java/com/hgcastle/controller/ExitController.java
@@ -1,7 +1,6 @@
 package com.hgcastle.controller;
 
 
-
 import com.hgcastle.dto.MemberDTO;
 import com.hgcastle.service.ExitService;
 import com.hgcastle.view.ExitResult;
@@ -13,35 +12,22 @@ public class ExitController {
     private final ExitService exitService;
     private final ExitResult exitResult;
 
-    public ExitController () {
+    public ExitController() {
 
         exitService = new ExitService();
         exitResult = new ExitResult();
     }
 
-    public void selectMemberId(Map<String, String> parameter){
+    public void selectMemberId(Map<String, String> parameter) {
 
         String id = parameter.get("id");
 
-        MemberDTO member = exitService.selectMemberId("id");
+        MemberDTO member = exitService.selectMemberId(id);
 
-        if (id != null){
+        if (member != null) {
             printResult.printMember(member);
-
-    public void exitMembersById(Map<String, String> parameter) {
-
-        String id = parameter.get("id");
-        MemberDTO memberDTO = new MemberDTO();
-        memberDTO.setId(id);
-        if(exitService.exitMembersById(memberDTO)) {
-            exitResult.printSuccessMessage("exitMemberSuccess");
         } else {
-            exitResult.printErrorMessage("exitMemberFail");
+            printResult.printErrorMessage("selectOne");
         }
-
     }
-
 }
-
-    public <__TMP__> __TMP__ selectId() {
-    }
